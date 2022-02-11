23c92f371679ac361c913568e7a67c7955e6df4e
【★༒✞︎©༒ʟɛɢɛռɖ✘-թπο】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-02-11 06:58:03 -0800

    Update and rename main (1).py to main.py

--- main.py
+++ main.py
@@ -1,37 +1,31 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 
-PYTHONBOT = """
+DRAGONBOT = """
 """
-print(PYTHONBOT)
+print(DRAGONBOT)
 print(
-    """String Generator. ==> PYTHONBot. Get Your Api Id & Api Hash From my.telegram.org and fill accordingly.
+    """String Generator. ==> DRAGONBot. Get Your Api Id & Api Hash From my.telegram.org and fill accordingly.
 ╭━━━┳╮╱╱╭┳━━━━┳╮╱╭┳━━━┳━╮╱╭╮MR_HACKER
 ┃╭━╮┃╰╮╭╯┃╭╮╭╮┃┃╱┃┃╭━╮┃┃╰╮┃┃
 ┃╰━╯┣╮╰╯╭┻╯┃┃╰┫╰━╯┃┃╱┃┃╭╮╰╯┃
 ┃╭━━╯╰╮╭╯╱╱┃┃╱┃╭━╮┃┃╱┃┃┃╰╮┃┃
 ┃┃╱╱╱╱┃┃╱╱╱┃┃╱┃┃╱┃┃╰━╯┃┃╱┃┃┃
-╰╯╱╱╱╱╰╯╱╱╱╰╯╱╰╯╱╰┻━━━┻╯╱╰━╯
-╭━━╮╭━━━┳━━━━╮LEGEND-LX
-┃╭╮┃┃╭━╮┃╭╮╭╮┃
-┃╰╯╰┫┃╱┃┣╯┃┃╰╯
-┃╭━╮┃┃╱┃┃╱┃┃
-┃╰━╯┃╰━╯┃╱┃┃
-╰━━━┻━━━╯╱╰╯PRAWASH_MALVIYA""")
-print("Real stick pythonbot newlock")
+╰╯╱╱╱╱╰╯╱╱╱╰╯╱╰╯╱╰┻━━━┻╯╱╰━╯PRAWASH_MALVIYA""")
+print("Real stick Dragonbot~Pro newlock")
 APP_ID = int(input("Enter APP ID - "))
 API_HASH = input("Enter API HASH - ")
 
-with TelegramClient(StringSession(), APP_ID, API_HASH) as PYTHONBOT:
+with TelegramClient(StringSession(), APP_ID, API_HASH) as DRAGONBOT:
     print("")
     print("This is your STRING_SESSION. Please Keep It safe.")
     print("")
-    print(PYTHONBOT.session.save())
+    print(DRAGONBOT.session.save())
     print("")
     print(
         "You can Get Your String Session In Saved Message of Your Telegram Account. Remember To Make New String Session Whenever You Terminate Sessions."
     )
-    omk = PYTHONBOT.send_message("me", f"`{PYTHONBOT.session.save()}`")
+    omk = DRAGONBOT.send_message("me", f"`{DRAGONBOT.session.save()}`")
     omk.reply(
-        "The above is the `PYTHON_STRING` #POWERFUL [PYTHONBOT](https://t.me/Python_Userbot_Support)"
+        "The above is the `DRAGON_STRING` #POWERFUL [DRAGONBOT~PRO](https://t.me/DragonPro_UserBot)"
     )
